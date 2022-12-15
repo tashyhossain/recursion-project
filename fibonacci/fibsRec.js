09ebf4043c3df09872a2a7598685b04618f2378b
Tash Hossain <tashyhossain@gmail.com>
2022-12-15 03:25:26 -0500

    Added mergeSort solve

--- fibonacci/fibsRec.js
+++ fibonacci/fibsRec.js
@@ -1,3 +1,13 @@
 function fibsRec(n) {
   return (n < 2) ? n : fibsRec(n - 2) + fibsRec(n - 1)
 }
+
+function fibsMemo(n, memory = {}) {
+  if (memory[n]) return memory[n]
+  if (n < 2) return memory[n] = n
+
+  memory[n] = fibsMemo(n - 2, memory) + fibsMemo(n - 1, memory) 
+  console.log(memory) 
+  return memory[n]
+}
+
--- merge-sort/mergeSort.js
+++ merge-sort/mergeSort.js
@@ -1,5 +1,24 @@
 function mergeSort(array) {
+  function merge(left, right) {
+    let current, sorted = []
 
+    while (left.length && right.length) {
+      if (left[0] < right[0]) {
+        current = left.shift()
+      } else {
+        current = right.shift()
+      }
+
+      sorted.push(current)
+    }
+
+    return sorted.concat(left, right)
+  }
+
+  if (array.length < 2) return array
+
+  let left = mergeSort(array.slice(0, array.length / 2))
+  let right = mergeSort(array.slice(array.length / 2))
+
+  return merge(left, right)
 }
-
-
